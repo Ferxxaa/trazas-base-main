6366854cc24a0be91c061037c3399bd54c3718af
Ferxxaa <gonzalezfernanda491@gmail.com>
2025-04-22 13:34:40 -0400

    prueba

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // Manual chunking para separar las dependencias de node_modules en un archivo 'vendor'
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-          // Se puede añadir más reglas de chunking si se requiere
-          if (id.includes('src/components/')) {
-            return 'components';
-          }
-        }
-      }
-    },
-    // Ajuste del límite de tamaño para advertencias de chunks grandes (en kilobytes)
-    chunkSizeWarningLimit: 1000, // Puedes ajustarlo a lo que necesites
+  optimizeDeps: {
+    include: ['crypto-browserify']
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify'
+    }
   }
-})
+});
