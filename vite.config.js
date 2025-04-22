81639d8fa53df3bb0a7d438c4233295b0028e5cb
Ferxxaa <gonzalezfernanda491@gmail.com>
2025-04-22 12:55:41 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Manual chunking: separa las dependencias de node_modules en un archivo llamado 'vendor'
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    // Ajustar el límite de tamaño para advertencias de chunks grandes (en kilobytes)
+    chunkSizeWarningLimit: 1000, // Puedes ajustar el límite a lo que necesites
+  }
 })
